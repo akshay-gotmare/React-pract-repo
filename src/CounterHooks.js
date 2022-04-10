73b8d5fd15bfe28ea2cf184a7f0130532c0017b4
Akshay Gotmare <akshaygotmare4@gmail.com>
2022-04-10 17:21:27 +0530

    First commit

---
+++ src/CounterHooks.js
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "./App";
+
+export default function CounterHooks(props) {
+  const [count, setCount] = useState(props.initialCount);
+  const style = useContext(ThemeContext);
+
+  const changeCount = (amount) => {
+    setCount((prevCount) => prevCount + amount);
+  };
+  return (
+    <div>
+      {/* <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button> */}
+      <button style={style} onClick={() => changeCount(-1)}>
+        -
+      </button>
+      <span>{count}</span>
+      <button style={style} onClick={() => changeCount(1)}>
+        +
+      </button>
+      {/* <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button> */}
+    </div>
+  );
+}
